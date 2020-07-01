01032bb25747c6ba02464a9ea7ad5a69d1e71d02
Christian Sheen <christiansheen@Christians-MacBook-Pro.local>
2020-07-01 02:57:55 -0400

    created Header component, NBD

---
+++ components/Header.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const Header = (props) => {
+	return (
+		<View style={styles.header}>
+			<Text style={styles.headerTitle}>{props.title}</Text>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	header: {
+		width: "100%",
+		height: 100,
+		paddingTop: 36,
+		backgroundColor: "#909FD4",
+		alignItems: "center",
+		justifyContent: "center",
+		shadowColor: "#000",
+		shadowOffset: {
+			width: 10,
+			height: 1,
+		},
+		shadowOpacity: 0.5,
+		shadowRadius: 10,
+	},
+	headerTitle: {
+		color: "#E8E8E8",
+		fontSize: 20,
+	},
+});
+
+export default Header;
